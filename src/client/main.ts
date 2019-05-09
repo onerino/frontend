31ecd0f00f9e5090fb4855650138126a7fca3de1
Akito Hataguchi <hataguchi@motionpicture.jp>
2019-05-09 11:44:19 +0900

    cinerino-frontendと同期

--- src/client/main.ts
+++ src/client/main.ts
@@ -7,10 +7,19 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+/**
+ * タイムゾーン設定
+ */
 import * as momentTimezone from 'moment-timezone';
-
 momentTimezone.tz.setDefault('Asia/Tokyo');
 momentTimezone.locale('ja');
+
+/**
+ * 言語設定
+ */
+import { defineLocale } from 'ngx-bootstrap/chronos';
+import { jaLocale } from 'ngx-bootstrap/locale';
+defineLocale('ja', jaLocale);
 
 if (environment.production) {
     enableProdMode();
